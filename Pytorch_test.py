01624a81fe0fd05bfdfda7c1f6dc20ef914d44e6
arprakash27 <arprakash@outlook.com>
2024-05-05 22:21:17 +0530

    first checkin of code which is working in pycharm environment

--- Pytorch_test.py
+++ Pytorch_test.py
@@ -1,23 +1,26 @@
 # For tips on running notebooks in Google Colab, see
 # https://pytorch.org/tutorials/beginner/colab
 
-#get_ipython().run_line_magic('matplotlib', 'inline')
-#%matplotlib inline
+# get_ipython().run_line_magic('matplotlib', 'inline')
+# %matplotlib inline
 
-import torchvision.transforms as transforms
-import numpy as np
-import json
-import requests
 
+# import numpy as np
+# import json
+# import requests
 import torch
 import PIL
 from PIL import Image
 import matplotlib.pyplot as plt
 import warnings
 import time
+import glob
+import torchvision.transforms as transforms
+# from torchvision import datasets, transforms
+
+
 warnings.filterwarnings('ignore')
 plt.show()
-
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 print(f'Using {device} for inference')
 
@@ -25,8 +28,6 @@ resnet50 = torch.hub.load('pytorch/vision', 'resnet50', pretrained=True)
 utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_convnets_processing_utils')
 
 resnet50.eval().to(device)
-
-import glob
 
 imgs_ = glob.glob("Images/*.jp*g")
 
@@ -38,7 +39,6 @@ for img in imgs_:
         print(img)
 
 
-from torchvision import datasets, transforms
 start = time.time()
 batch = torch.cat([utils.prepare_input_from_uri(img) for img in imgs_]).to(device)
 
@@ -53,8 +53,9 @@ torch.save(resnet50.state_dict(), 'saved_model.pth')
 
 for img, result in zip(imgs_, results):
     img = PIL.Image.open(img)
-    img.thumbnail((256, 256), Image.LANCZOS)
+    # img.thumbnail((256, 256), Image.LANCZOS)
     plt.imshow(img)
+    plt.text(0,0,result)
     plt.show()
-    print(result)
+    # print(result)
 print("Average Inference time per image:", (end-start)/6, "s")
